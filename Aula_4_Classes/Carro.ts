52c37519d97422689172eb5e12c6bccfe68d0a50
anisio-ifsp <160257188+anisio-ifsp@users.noreply.github.com>
2024-03-06 20:55:18 -0300

    Update Carro.ts

--- Aula_4_Classes/Carro.ts
+++ Aula_4_Classes/Carro.ts
@@ -1,12 +1,9 @@
-// Definição da classe 'Carro'
 class Carro {
-    // Propriedades da classe
     marca: string;
     modelo: string;
     ano: number;
     cor: string;
   
-    // Construtor da classe
     constructor(marca: string, modelo: string, ano: number, cor: string) {
       this.marca = marca;
       this.modelo = modelo;
@@ -14,15 +11,12 @@ class Carro {
       this.cor = cor;
     }
   
-    // Método para exibir informações do carro
     exibirInfo(): void {
       console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}, Cor: ${this.cor}`);
     }
   }
   
-  // Criando uma instância da classe 'Carro'
   const meuCarro = new Carro("Toyota", "Corolla", 2020, "Prata");
   
-  // Exibindo informações do carro
   meuCarro.exibirInfo();
     
