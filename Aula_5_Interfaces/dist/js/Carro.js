fdf5fb5c5337ecb1ae49dac8c4ce3f679baec517
anisio002 <anisio002@gmail.com>
2024-03-15 14:07:12 -0300

    gitignore

---
+++ Aula_5_Interfaces/dist/js/Carro.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Carro = void 0;
+class Carro {
+    acelerar() {
+        console.log("Carro acelerando...");
+    }
+    parar() {
+        console.log("Carro parando...");
+    }
+}
+exports.Carro = Carro;
